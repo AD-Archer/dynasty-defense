3055be3eca0ee5cb876d28e77bcd83dd6b75696c
ad-archer <adarcher21@gmail.com>
2024-11-09 20:45:11 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React, { useState } from "react"; // Importing React and useState for state management
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Importing HashRouter, Route, and Routes for navigation
-import "./App.css"; // Importing the main CSS file for styling
-import Register from "./components/register"; // Importing the Register component
-import Login from "./components/login"; // Importing the Login component
-import HomePage from "./components/homepage"; // Importing the HomePage component
-import SettingsPage from "./components/SettingsPage"; // Adjust the import path
-import AdminLog from "./components/AdminLog"; // Make sure the casing is correct
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Register from "./components/register";
+import Login from "./components/login";
+import HomePage from "./components/homepage";
+import SettingsPage from "./components/SettingsPage";
+import AdminLog from "./components/AdminLog";
 
-// Defining the main App component
 function App() {
-  // Using useState to track whether the Login page is currently being shown
-  const [showLogin, setShowLogin] = useState(false); // Initially showing the Register page by default
+  const [showLogin, setShowLogin] = useState(false);
 
-  // Function to toggle between the Login and Register pages
   const togglePage = () => {
-    setShowLogin((prev) => !prev); // Toggle logic
+    setShowLogin((prev) => !prev);
   };
 
-  // Returning the main JSX layout for the App component
   return (
     <Router>
       <div className="App">
-        {/* Defining the routes for the application */}
         <Routes>
           <Route
             path="/"
@@ -35,12 +30,11 @@ function App() {
           />
           <Route path="/home" element={<HomePage />} />
           <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/AdminLog" element={<AdminLog />} />{" "}
-          {/* Added AdminLog route */}
+          <Route path="/AdminLog" element={<AdminLog />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-export default App; // Exporting the App component
+export default App;
